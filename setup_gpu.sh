74de8a4f27ecda8d53439de6f0d5d4950f15b421
Zekun Shi <shizk@sea.com>
2022-11-08 15:58:26 +0800

    update gpu setup

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -1,2 +1,2 @@
 pip uninstall jax jaxlib -y
-pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install --upgrade "jax[cuda]==0.3.23" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
